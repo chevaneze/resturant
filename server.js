e7ec06fb5acaf9ff9212c85218d4a2627100237d
moguera57 <josephbanwart@gmail.com>
2019-02-23 10:50:54 -0500

    postworks

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var tables = [
         name: "Testy McTestson",
         phone: 5555555555,
         email: "thisisatest@TextTrackList.test",
-        id: 0
+        id: 1
     }
 ]
 
@@ -30,6 +30,16 @@ app.get("/reserve", function(req, res) {
 app.get("/api/tables", function(req, res) {
     return res.json(tables);
 });
+
+app.post("/api/tables", function(req, res) {
+  
+    var newreserve = req.body;
+  
+    newreserve.id = tables.length+1
+    console.log(newreserve);
+    tables.push(newreserve);
+    res.json(newreserve);
+  });
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
