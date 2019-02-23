3325ce8d6f6d4bc7a9a92c38f3a47d125d878215
moguera57 <josephbanwart@gmail.com>
2019-02-23 09:54:49 -0500

    express

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
-var http = require("http");
+var express = require("express");
+var path = require("path");
 
-var PORT = 3000;
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
 
-function handleRequest(request, response) {
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-    response.end("It works!! Path Hit: " + request.url);
-
-}
-
-var server = http.createServer(handleREquest);
-
-server.listen(PORT, function() {
-    
-    console.log("Server listening on: http://localhost:" + PORT)
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
 });
-
